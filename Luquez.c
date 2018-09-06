800f34a8086aae792750036aea19b31c6edac60b
EliseoLuquez <leliseo89@hotmail.com>
2018-09-05 23:55:51 -0300

    commit inicial

---
+++ Luquez.c
@@ -0,0 +1,86 @@
+# include  "Luquez.h"
+# include  <stdio.h>
+
+/** \brief Suma de dos operandos
+ *
+ * \param a primero operando a sumar
+ * \param b segundo operando a sumar
+ * \return Total de la suma
+ *
+ */
+
+float suma(float a, float b)
+{
+    return (a+b);
+}
+
+
+/** \brief Resta de 2 operando
+ *
+ * \param a primer operando a restar
+ * \param b segundo operando a restar
+ * \return Total de la resta
+ *
+ */
+
+float resta(float a, float b)
+{
+    return (a-b);
+}
+
+
+/** \brief Multiplicacion de 2 operandos
+ *
+ * \param a primer operando a multiplicar
+ * \param b segundo operando a multiplicar
+ * \return Total de la multiplicacion
+ *
+ */
+
+float multiplicacion(float a, float b)
+{
+    return (a*b);
+}
+
+
+/** \brief Division de 2 operando
+ *
+ * \param a primer operando a dividir
+ * \param b segundo operando a dividir
+ * \return Total de la division
+ *
+ */
+
+float division(float a, float b)
+{
+    return a/b;
+}
+
+
+/** \brief Factorial de 1 operando
+ *
+ * \param numero operando a hacer factorial
+ * \return Total del factorial
+ *
+ */
+
+int factorial (int numero)
+{
+    int fact = 1;
+    if(numero==0)
+    {
+        return 1;
+    }
+    else if(numero<0)
+    {
+        return 0;
+    }
+    else
+    {
+        for(int i=1;i<=numero;i++)
+        {
+             fact*=i;
+        }
+    }
+    return fact;
+}
---
+++ main.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Luquez.h"
+
+
+int main()
+{
+    float x;
+    float y;
+    int flagA = 0;
+    int flagB = 0;
+    int menu;
+    int flagSalir = 0;
+    int flag3 = 0;
+
+    do
+    {
+        system("cls");
+        printf("Elija una opcion\n\n");
+        if(flagA==0)
+        {
+            printf("1. Ingresar 1er operando (A=x)\n");
+        }
+        else
+        {
+            printf("1. Ingresar 1er operando (A=%.2f)\n", x);
+        }
+        if(flagB==0)
+        {
+            printf("2. Ingresar 2do operando (B=y)\n\n");
+        }
+        else
+        {
+            printf("2. Ingresar 2do operando (B=%.2f)\n\n", y);
+        }
+        printf("3. Calcular operaciones: \na) Calcular la suma (A+B)\nb) Calcular la resta (A-B)\nc) Calcular la division (A/B)\n");
+        printf("d) Calcular la multiplicacion (A*B)\ne) Calcular el factorial (A!)\n");
+        printf("\n4. Mostrar resultados\n");
+        printf("5. Salir\n");
+        scanf("%d", &menu);
+
+        switch(menu)
+        {
+                case 1:
+                        system("cls");
+                        printf("1. Ingresar 1er operando (A=x)\n");
+                        fflush(stdin);
+                        scanf("%f", &x);
+                        flagA=1;
+                        break;
+                case 2:
+                        system("cls");
+                        printf("2. Ingresar 2do operando (B=y)\n)");
+                        fflush(stdin);
+                        scanf("%f", &y);
+                        flagB=1;
+                        break;
+                case 3:
+                        if(flagA!=1)
+                        {
+                            printf("Debe ingresar operando A\n");
+                            printf("1. Ingresar 1er operando (A=x)\n");
+                            fflush(stdin);
+                            scanf("%f", &x);
+                            flagA=1;
+                        }
+                        if(flagB!=1)
+                        {
+                            printf("Debe ingresar operando B\n");
+                            printf("2. Ingresar 2do operando (B=y)\n)");
+                            fflush(stdin);
+                            scanf("%f", &y);
+                            flagB=1;
+                        }
+                        system("cls");
+                        printf("\ncalculando...\n\n");
+                        suma(x, y);
+                        resta(x, y);
+                        division(x, y);
+                        multiplicacion(x, y);
+                        factorial(x);
+                        factorial(y);
+                        flag3 = 1;
+                        system("pause");
+                        break;
+
+                case 4:
+                        if(flag3!=1)
+                        {
+                            printf("Primero debe hacer los calculos con la opcion 3\n");
+                            system("pause");
+                        }
+                        else
+                        {
+                            system("cls");
+                            printf("Resultados:\n");
+                            printf("\n1er operando (A=%.2f)\n", x);
+                            printf("2do operando (B=%.2f)\n\n", y);
+                            printf("El resultado de A+B es: %.2f", suma(x, y));
+                            printf("\nEl resultado de A-B es: %.2f", resta(x, y));
+                        }
+                        if(y==0)
+                        {
+                            printf("\nEl resultado de A/B es: No es posible dividir por cero!");
+                        }
+                        else
+                        {
+                            printf("\nEl resultado de A/B es: %.2f", division(x, y));
+                        }
+                        printf("\nEl resultado de A*B es: %.2f", multiplicacion(x, y));
+                        printf("\nEl factorial de A es: %d", factorial(x));
+                        printf("\nEl factorial de B es: %d\n\n", factorial(y));
+                        system("pause");
+                        break;
+                case 5:
+                        flagSalir=1;
+                        printf("\nAprete Enter para salir\n");
+                        break;
+        }
+
+    }
+    while(flagSalir==0);
+
+    return 0;
+}
+
+
+
